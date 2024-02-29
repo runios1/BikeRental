c68ab8b9b4453726ba676c8c72bd772a77296fcb
Run <runios123@gmail.com>
2024-02-29 21:55:12 +0200

    Add form submission functionality to upload.html

---
+++ src/js/uploadFormSubmit.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const uploadForm = document.querySelector("form");
+  const errorContainer = document.getElementById("errorContainer");
+
+  uploadForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const formData = new FormData(uploadForm);
+    fetch("http://localHost:5500/bikes/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Upload failed. Please try again.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Upload Success:", data);
+        window.location.href = "/BikeRental/src/html/myBikes.html";
+      })
+      .catch((error) => {
+        console.error("Upload Error:", error);
+        errorContainer.textContent = error.message;
+      });
+  });
+});
